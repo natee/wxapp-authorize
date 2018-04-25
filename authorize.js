4e39fe726cda484fe6165d4ee2852c4247d973b9
ZhaoKe <kerncink@gmail.com>
2018-04-25 16:10:04 +0800

    之前的方式发现授权未阻断后续操作，更新一版

--- authorize.js
+++ authorize.js
@@ -1,74 +1,74 @@
-let code
+import api from '@/api'
 
-const storeUserInfo = (params) => {
-  console.log('待对比数据：', params)
-  wx.request({
-    url: '/api/xxx',
-    method: 'POST',
-    data: params,
-    success: function (res) {
-      console.log('success', res)
-    },
-    fail: function (res) {
-      console.log('fail', res)
-    }
-  })
-}
+class Authorize {
+  constructor(){
+    this.callback = null
+    this.code = null
+  }
 
-const getUserInfo = (code) => {
-  wx.getUserInfo({
-    success(data) {
-      storeUserInfo({
-        code: code,
-        info: data
-      })
-    },
-    fail(res) {
-      console.log('getUserInfo：用户主动拒绝授权')
-      authorizeFail()
-    }
-  })
-}
-
-// 重新打开授权
-const reAuthorize = (code) => {
-  wx.openSetting({
-    success(res) {
-      if (res.authSetting['scope.userInfo']) {
-        getUserInfo()
-      } else {
-        // 妈蛋，敢拒绝，必须授权！
-        authorizeFail()
+  authroize(callback){
+    this.callback = callback
+    wx.login({
+      success: (res) => {
+        this.code = res.code
+        this.getUserInfo()
       }
-    }
-  })
-}
+    })
+  }
 
-const authorizeFail = () => {
-  wx.showModal({
-    title: "用户未授权",
-    content: "不授权将影响您领取红包，点击“确定”打开授权设置，点击“取消”退出小程序",
-    confirmText: "确定",
-    success(res) {
-      if (res.confirm) {
-        reAuthorize()
-      } else if (res.cancel) {
-        // 由于前面没有页面，所以这里等于退出小程序
-        wx.navigateBack()
+  getUserInfo(){
+    wx.getUserInfo({
+      success: (data) => {
+        
+        // 这一段是用来记录用户信息，实际当中不一定有
+        api.saveUserInfo({
+          code: this.code,
+          info: JSON.stringify(data.userInfo)
+        }).then(res => {
+          if(res.code === 0){
+            
+            // 如果需要在授权成功后才进行后续操作，传入callback
+            if(this.callback && typeof this.callback === "function"){
+              this.callback()
+            }
+          }
+        })
+      },
+      fail: (res) => {
+        console.log('getUserInfo：用户主动拒绝授权')
+        this.authorizeFail()
       }
-    }
-  })
+    })
+  }
+
+  authorizeFail(){
+    wx.showModal({
+      title: "用户未授权",
+      content: "不授权将影响您领取红包，点击“确定”打开授权设置，点击“取消”退出小程序",
+      confirmText: "确定",
+      success: (res) => {
+        if (res.confirm) {
+          this.reAuthorize()
+        } else if (res.cancel) {
+          // 由于前面没有页面，所以这里等于退出小程序
+          wx.navigateBack()
+        }
+      }
+    })
+  }
+
+  reAuthorize(){
+    wx.openSetting({
+      success: (res) => {
+        if (res.authSetting['scope.userInfo']) {
+          this.getUserInfo()
+        } else {
+          // 妈蛋，敢拒绝，必须授权！
+          this.authorizeFail()
+        }
+      }
+    })
+  }
 }
 
-const mpAuthorize = (config, callback) => {
-  wx.login({
-    success(res) {
-      code = res.code
-      getUserInfo()
-    }
-  })
-}
-
-export {
-  mpAuthorize
-}
+export default Authorize
